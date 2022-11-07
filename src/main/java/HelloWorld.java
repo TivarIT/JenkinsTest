4f3280574c6dfce73c8d4109d3d8a6ce0a0bfe3b
Tsimafei Novik <timoxa.novik03@gmail.com>
2022-11-07 15:28:15 +0300

    Jenkins, hear me!

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("A new world has been created!");
         System.out.println("Jenkins, hear me!");
+        System.out.println("Jenkins, hear me!");
     }
 }
