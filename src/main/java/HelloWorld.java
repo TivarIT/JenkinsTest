11f59a613f0241e8721301b72b4b8013a7127edf
Tsimafei Novik <timoxa.novik03@gmail.com>
2022-11-07 15:17:09 +0300

    first commit

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("A new world has been created!");
     }
 }
